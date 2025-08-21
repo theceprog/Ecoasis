9826e19ff631b0efd608d1168ece4169a7a08233
WeaKaDiKa <weakadika@gmail.com>
2025-08-21 09:25:58 +0800

    dd

--- app/src/main/java/com/nu/ecoasis/MainActivity.kt
+++ app/src/main/java/com/nu/ecoasis/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.nu.ecoasis
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        updateButtonStates()
+    }
+
+    private fun updateButtonStates() {
+        val isNightMode = isNightModeEnabled()
+
+        findViewById<ImageButton>(R.id.up_pump).isActivated = isNightMode
+        findViewById<ImageButton>(R.id.down_pump).isActivated = isNightMode
+    }
+
+    private fun isNightModeEnabled(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+    }
+
+    // Handle theme changes
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        updateButtonStates()
     }
 }
